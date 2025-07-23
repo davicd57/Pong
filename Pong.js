bf1af1981394a5aedc513feadc42b912bfdfc357
davicd57 <davibr497@gmail.com>
2025-07-23 17:53:41 -0300

    Pong.js

--- Pong.js
+++ Pong.js
@@ -1,15 +1,4 @@
-var player=document.getElementById("player");
 var position=190;
-var position_ball=0;
-var ball=document.getElementById("ball");
-var time=1;
-var score=document.getElementById("score");
-var points_rival=0;
-var your_points=0;
-
-score.innerHTML="Score"+your_points+"x"+points_rival
-
-
 function left(){
     position-=10;
     player.style.left=position+"px";
@@ -20,31 +9,63 @@ function right(){
     player.style.left=position+"px";
 }
 
-function game(){
-    var playerRect=player.getBoundingClientRect();
-    var ballRect=ball.getBoundingClientRect();
-    position_ball+=1
-    ball.style.top=position_ball+"px";
-    setTimeout(game,time); 
+document.addEventListener("keydown", function(event) {
+  if (event.key === "ArrowLeft") {
+    left();
+  } else if (event.key === "ArrowRight") {
+    right();
+  }
+});
 
-    if (ball.style.top=="670px"){
-        position_ball = 0;
-        ball.style.top = "0px";
-        points_rival+=1;
-        score.innerHTML="Score:0x"+points_rival;
+function start(){
+    var player=document.getElementById("player");
+    var position_ball=0;
+    var ball=document.getElementById("ball");
+    var time=1;
+    var score=document.getElementById("score");
+    var points_rival=0;
+    var your_points=0;
+    score.innerHTML="Score"+your_points+"x"+points_rival
+
+    function game(){
+        var playerRect=player.getBoundingClientRect();
+        var ballRect=ball.getBoundingClientRect();
+        position_ball+=2
+        ball.style.top=position_ball+"px";
+        setTimeout(game,time); 
+
+        if (ball.style.top=="670px"){
+            position_ball = 0;
+            ball.style.top = "0px";
+            var ball_spawn=Math.floor(Math.random()*450)+1;
+            ball.style.left=ball_spawn+"px";
+            points_rival+=1;
+            score.innerHTML="Score:"+your_points+"x"+points_rival
+
+            if (your_points==10){
+                position_ball=0
+                console.log("vc venceu")
+            }
+        }
+
+        if (ballRect.bottom>=playerRect.top &&
+            ballRect.left<playerRect.right &&
+            ballRect.right>=playerRect.left
+        ){
+            position_ball = 0;
+            ball.style.top = "0px";
+            var ball_spawn=Math.floor(Math.random()*450)+1;
+            ball.style.left=ball_spawn+"px";
+            your_points+=1;
+            score.innerHTML="Score:"+your_points+"x"+points_rival
+
+            if (points_rival==10){
+                position_ball=0
+                console.log("vc perdeu")
+            }
+        }
+
     }
-
-    if (ballRect.bottom>=playerRect.top &&
-        ballRect.left<playerRect.right &&
-        ballRect.right>=playerRect.left
-    ){
-        position_ball = 0;
-        ball.style.top = "0px";
-        your_points+=1;
-        score.innerHTML="Score:"+your_points+"x0";
+    game()
     }
-    
-
-
-}
-game()
+start()
